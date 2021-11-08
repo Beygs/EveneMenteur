8deacbbbd4b36aa15653b52a8e0b49eca27fc603
Beygs <boris.gilles@hotmail.fr>
2021-11-08 15:51:54 +0100

    Email changed in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,14 @@ Event.destroy_all
 Attendance.destroy_all
 
 20.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = "#{first_name.downcase}-#{last_name.downcase}@yopmail.com"
+
   User.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email
+    first_name: first_name,
+    last_name: last_name,
+    email: email
   )
 end
 
