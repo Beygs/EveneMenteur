06d9cbf7f9cf547c39b990092b30cf900300bd93
Beygs <boris.gilles@hotmail.fr>
2021-11-08 19:38:29 +0100

    Attendance mail done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,14 @@ class UserMailer < ApplicationMailer
     @url = 'https://evenementeur.com/login'
     mail(from: ENV['EMAIL_FROM'], to: @user.email, subject: 'Bienvenue !')
   end
+
+  def attendance_email(attendance)
+    @event = attendance.event
+    @participant = attendance.user
+    mail(
+      from: ENV['EMAIL_FROM'],
+      to: @event.admin.email,
+      subject: "Nouvelle participation à l'événement #{@event.title} 🎉"
+    )
+  end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,12 @@
 class Attendance < ApplicationRecord
+  after_create :send_attendance
+
   belongs_to :user
   belongs_to :event
+
+  private
+
+  def send_attendance
+    UserMailer.attendance_email(self).deliver_now
+  end
 end
